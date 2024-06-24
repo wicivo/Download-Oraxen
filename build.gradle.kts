8eb4e8dc94939dc4ad77c61269391108b791854f
wicivo <116191734+wicivo@users.noreply.github.com>
2024-06-24 10:51:35 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,8 @@ dependencies {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
